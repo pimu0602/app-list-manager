7d0c5fa3fa48b2cfcd1c73089298971937da8b74
pimu0602 <git>
2026-06-22 10:33:30 +0900

    Configure Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Firebase コンソールの「プロジェクトの設定」→「マイアプリ」から取得した値へ置き換えてください。
 // apiKey はクライアント向け識別情報です。データ保護は firestore.rules で行います。
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBCd0LjKT2iW2smc6Ale3057nEIDZaIBug",
+  authDomain: "app-list-manager.firebaseapp.com",
+  projectId: "app-list-manager",
+  storageBucket: "app-list-manager.firebasestorage.app",
+  messagingSenderId: "830933047499",
+  appId: "1:830933047499:web:0a7471b254614bc96e634e"
 };
